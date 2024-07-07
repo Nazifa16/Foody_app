c8f0dd082ef0dd7dbd795319f04155ec481850b4
Nazifa16 <nazifa.abdullayeva@gmail.com>
2024-07-08 00:58:19 +0400

    start project

---
+++ pages/admin/login/index.tsx
@@ -0,0 +1,23 @@
+
+import Image from 'next/image'
+import React from 'react'
+
+const AdminLogin = () => {
+    return (
+        <>
+
+            <div>
+                <nav className='logo bg-black  mb-20  pl-8 md:mb-44' >
+                    <Image width={102} height={22} src="/adminFoody.svg" alt='foodyLogo' />
+
+
+
+                </nav>
+
+            </div>
+        </>
+
+    )
+}
+
+export default AdminLogin
